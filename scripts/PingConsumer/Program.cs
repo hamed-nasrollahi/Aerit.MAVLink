b408f516c75c5aa6955710ff2bed02a1ce83ed11
Pablo Belin <pablo.belin@gmail.com>
2021-06-07 12:55:10 +0200

    Test script added

---
+++ scripts/PingConsumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+using Aerit.MAVLink;
+
+using var client = new UdpClient(4001);
+
+while (true)
+{
+    var frame = await client.ReceiveAsync();
+
+    if (frame.Length > 0)
+    {
+        if (frame.Buffer[0] == Magic.V2)
+        {
+            var packet = V2.Packet.Deserialize(frame.Buffer);
+
+            if (packet.Validate(Ping.MessageCRCExtra))
+            {
+                var ping = Ping.Deserialize(packet.Payload);
+            }
+        }
+    }
+}
